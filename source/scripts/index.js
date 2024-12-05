a155acdb5842582301a2a7e0a878bc9c7405607d
OlegSulitskii <sulitsky2011@yandex.ru>
2024-12-06 02:34:47 +0400

    Завершает мелкие сетки и адаптивные декоративные элементы

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,5 +1,13 @@
 const navMain = document.querySelector('.navigation');
+const buttonToggle = document.querySelector('.button-toggle');
 const navToggle = document.querySelector('.header__toggle');
+
+navMain.classList.remove('navigation--nojs');
+navMain.classList.remove('navigation--open');
+navMain.classList.add('navigation--close');
+buttonToggle.classList.add('button-toggle--open');
+buttonToggle.classList.remove('button-toggle--nojs');
+
 
 navToggle.addEventListener('click', () => {
   if (navMain.classList.contains('navigation--close')) {
